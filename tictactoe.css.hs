07d8e478bb1669dba3e97f9c738368e852760549
Thomas Levine <occurrence@thomaslevine.com>
2013-04-27 11:38:45 -0400

    ?

--- tictactoe.css.hs
+++ tictactoe.css.hs
@@ -1,2 +1,42 @@
-data Cell = X | O | Empty deriving (Enum)
-type Board = (Cell,Cell,Cell,Cell,Cell,Cell,Cell,Cell,Cell)
+
+-- Types
+data Side = X | O | Empty deriving (Enum)
+type Board = Board {
+  zero :: Side,
+  one :: Side,
+  two :: Side,
+  three :: Side,
+  four :: Side,
+  five :: Side,
+  six :: Side,
+  seven :: Side,
+  eight :: Side
+}
+
+fromSide :: Side -> Char
+fromSide X = 'x'
+fromSide O = 'o'
+fromSide Empty = '_'
+
+fromBoard :: Board -> String
+fromBoard b = [ fromSide (zero b),
+                fromSide (one b),
+                fromSide (two b),
+                fromSide (three b),
+                fromSide (four b),
+                fromSide (five b),
+                fromSide (six b),
+                fromSide (seven b),
+                fromSide (eight) ]
+
+-- Proerties
+winner :: Board -> Side
+winner = undefined
+
+turn :: Board -> Side
+turn = undefined
+
+-- CSS
+cssTurn :: Board -> String
+cssTurn b = "#board." ++ (fromBoard b) ++ " .five:after { content: '" ++ x ++ "'; }"
+
