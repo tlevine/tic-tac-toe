f38252c84106bb9d2559f5b3448bd90de1a28931
Thomas Levine <occurrence@thomaslevine.com>
2013-05-01 20:10:42 -0400

    add clickers

--- tictactoe.js
+++ tictactoe.js
@@ -2,10 +2,10 @@
   var whose_turn = function(className) {
       return ['x', 'o'][className.split('_').filter(function(a) { return a != ''; }).length % 2]
   }
-  var cells = {
-      'cell zero': 0, 'cell one': 1, 'cell two': 2,
-      'cell three': 3, 'cell four': 4, 'cell five': 5,
-      'cell six': 6, 'cell seven': 7, 'cell eight': 8
+  var clickers = {
+      'clicker zero': 0, 'clicker one': 1, 'clicker two': 2,
+      'clicker three': 3, 'clicker four': 4, 'clicker five': 5,
+      'clicker six': 6, 'clicker seven': 7, 'clicker eight': 8
   }
   var n, i, className
   
@@ -14,9 +14,9 @@
       return this.substr(0, index) + character + this.substr(index+character.length);
   }
   
-  for (cell in cells){
-      document.getElementsByClassName(cell)[0].addEventListener('click', function(e) {
-          i = cells[e.target.className]
+  for (clicker in clickers){
+      document.getElementsByClassName(clicker)[0].addEventListener('click', function(e) {
+          i = clickers[e.target.className]
           className = document.getElementById('board').className
           if (className[i] == '_') {
               document.getElementById('board').setAttribute('class', className.replaceAt(i, whose_turn(className)))
